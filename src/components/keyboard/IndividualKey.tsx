0f9967d8462afa94788360ccf7e8e5f0a7a4a9d1
brynsroberts <bryan.roberts.home@gmail.com>
2022-02-14 20:45:17 -0800

    update enter button

--- src/components/keyboard/IndividualKey.tsx
+++ src/components/keyboard/IndividualKey.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import { IoReturnDownForwardSharp } from "react-icons/io5";
+// import { IoReturnDownForwardSharp } from "react-icons/io5";
 import { BsBackspace } from "react-icons/bs";
 
 import "./IndividualKey.css";
@@ -22,8 +22,7 @@ const IndividualKey: React.FC<KeyProps> = (props) => {
       disabled={props.disableButton}
       size="sm"
     >
-      {props.letter !== "ENTER" && props.letter !== "DELETE" && props.letter}
-      {props.letter === "ENTER" && <IoReturnDownForwardSharp />}
+      { props.letter !== "DELETE" && props.letter}
       {props.letter === "DELETE" && <BsBackspace />}
     </Button>
   );
